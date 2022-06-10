13613756b4f31d1e16ca67534d17eac98fc5d51b
chenkaiyuan <chenky@126.com>
2022-06-10 08:22:36 +0800

    main

--- assets/data/biography.js
+++ assets/data/biography.js
@@ -1,4 +1,4 @@
 const biography = {
-    "en": ``,
-    "cn": `陈凯源，清华大学软件学院研究生，师从<a href='https://scholar.google.com/citations?user=eUbmKwYAAAAJ'>龙明盛副教授</a>。在清华大学数学系成本科，并获得理学学士学位。主要研究深度图像生成模型，深度学习驱动的气象预报技术。`
+    "en": `I am a master student at School of Software, Tsinghua University, advised by associate professor Mingsheng long. I finished bachelor's degree in mathematics from Tsinghua University. My research interest is deep video generation and deep learning based weather forecasting `,
+    "cn": `陈凯源，清华大学软件学院研究生，师从<a href='https://scholar.google.com/citations?user=_MjXpXkAAAAJ&hl=zh-CN'>龙明盛副教授</a>。在清华大学数学系成本科，并获得理学学士学位。主要研究深度图像生成模型，深度学习驱动的气象预报技术。`
 };
